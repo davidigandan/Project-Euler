be642730622a2dbeafb638afde5e8c48bda8ae74
davidigandan <69117955+davidigandan@users.noreply.github.com>
2022-08-21 19:18:07 +0100

    added linked list framework

--- src/Problem 81.py
+++ src/Problem 81.py
@@ -1,16 +1,106 @@
-# https://projecteuler.net/problem=81
-#Path sum: two ways
-#Using only translations of 1 right and 1 down, find the value of theshortest path from the top left to the bottom right of problem81matrix.txt
+import numpy as np
 
-#imports the txt matrix file
-f=open('problem81 matrix.txt','r')
-content=f.read()
-print(content)
-f.close()
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        
+
+class linked_list:
+    def __init__(self):
+        self.head=None
+        self.count=0
+
+    def insertEnd(self, newNode):
+        if self.head is None:
+            self.head=newNode
+        else:
+            lastNode=self.head
+            while True:
+                if lastNode.next is None:
+                    break
+                lastNode=lastNode.next
+            lastNode.next=newNode
+
+    def insertHead(self, newNode):
+        tempNode = self.head
+        self.head = newNode         
+        self.head.next = tempNode        
+        #del tempNode
 
 
-#or i in content:
-    #print (i)
+    def insertAt(self, newNode, position):
+        currentNode=self.head
+        currentPosition=0
+        while True:
+            if currentPosition == position:
+                previousNode.next=newNode
+                newNode.next=currentNode
+                break
+            previousNode=currentNode
+            currentNode=currentNode.next
+            currentPosition += 1
 
-#read each line of the txt file and add it to a line on an array of an array
-#print (super_matrix)
+    def deleteEnd(self):
+        lastNode=self.head
+        while lastNode.next is not None:
+            prevNode=lastNode
+            lastNode=lastNode.next
+        prevNode.next=None
+
+    
+
+    def deleteAt(self, position):
+        currentNode=self.head
+        currentPosition=0
+        while True:
+            if currentPosition == position:
+                prevNode.next=currentNode.next
+                currentNode.next=None
+                break
+            prevNode=currentNode
+            currentNode=currentNode.next
+            currentPosition +=1
+
+          
+            
+
+    def isEmpty(self):
+        if self.head is None:
+            return True
+        else:
+            return False
+
+    def traversal(self):
+        currentNode=self.head
+        while currentNode is not None:
+            print(currentNode.data)
+            currentNode=currentNode.next
+    
+
+    def deleteHead(self):
+        if self.isEmpty() is False:
+            prevHead=self.head
+            self.head=self.head.next
+            prevHead.next=None
+            print("The first item is deleted successfully")
+        else:
+            print("Linked List is empty, Delete Failed")
+      
+    
+FirstNode=Node(2)
+mylist=linked_list()
+mylist.insertEnd(FirstNode)
+
+SecondNode=Node(3)
+mylist.insertEnd(SecondNode)
+
+ThirdNode=Node(4)
+mylist.insertEnd(ThirdNode)
+
+FourthNode=Node(1)
+mylist.insertHead(FourthNode)
+
+mylist.deleteHead()
+mylist.traversal()
+
